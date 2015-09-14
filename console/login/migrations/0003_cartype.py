add16025c0e818c0d2cb97e552a1d3d4606d06b0
anoopms1988 <1988anoopms@gmail.com>
2015-09-14 21:25:55 +0530

    files

---
+++ console/login/migrations/0003_cartype.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('login', '0002_auto_20150914_0605'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='CarType',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('type', models.CharField(max_length=100)),
+                ('created_at', models.DateTimeField(auto_now_add=True, null=True)),
+                ('updated_at', models.DateTimeField(auto_now=True, null=True)),
+            ],
+            options={
+                'db_table': 'cartypes',
+            },
+        ),
+    ]
