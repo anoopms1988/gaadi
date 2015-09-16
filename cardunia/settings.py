8df16574318b5ffcbdf8bc2dc5d7393bdc6db681
anoopms1988 <1988anoopms@gmail.com>
2015-09-16 22:03:11 +0530

    fixed untracked files

--- cardunia/settings.py
+++ cardunia/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'cardunia',
         'USER': 'postgres',
-        'PASSWORD': 'inapp',
+        'PASSWORD': 'postgres',
         'HOST': 'localhost'
     }
 }
