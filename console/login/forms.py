720bef6cd2a6ea7ff021725359dd7c2958c19052
anoopms <anoop.ms@inapp.com>
2015-09-16 16:55:34 +0530

    Edit car

--- console/login/forms.py
+++ console/login/forms.py
@@ -1,8 +1,9 @@
 from django import forms
-from .models import Car,Company,CarType
+from .models import Car, Company, CarType
 
 
 class LoginForm(forms.Form):
+	
     username = forms.CharField(error_messages={'required': 'Username is required'}, required=True, label='Username',
                                max_length=100,
                                widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
@@ -12,16 +13,18 @@ class LoginForm(forms.Form):
 
 class CarForm(forms.ModelForm):
     company = forms.ChoiceField(error_messages={'required': 'Company name is required'},
-                                choices=[('', 'Select')] + [(o.id, o.name) for o in Company.objects.all()],
-                                required=True, widget=forms.Select(attrs={'class': 'form-control'}))
+                                choices=[('', 'Select')] + [(o.id, o.name)
+                                                            for o in Company.objects.all()],
+                                required=True, widget=forms.Select(attrs={'class': 'form-control '}))
     cartype = forms.ChoiceField(error_messages={'required': 'Cartype is required'},
-                                choices=[('', 'Select')] + [(o.id, o.cartype) for o in CarType.objects.all()],
+                                choices=[('', 'Select')] + [(o.id, o.cartype)
+                                                            for o in CarType.objects.all()],
                                 required=True, widget=forms.Select(attrs={'class': 'form-control'}))
     name = forms.CharField(error_messages={'required': 'Car name is required'}, required=True, max_length=100,
                            widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter car name'}))
-    description = forms.CharField(required=False,widget=forms.Textarea(
+    description = forms.CharField(required=False, widget=forms.Textarea(
         attrs={'class': 'form-control', 'placeholder': 'Enter car description'}))
 
     class Meta:
         model = Car
-        fields = ['name', 'company', 'cartype']
+        fields = ['name', 'company', 'cartype', 'description']
