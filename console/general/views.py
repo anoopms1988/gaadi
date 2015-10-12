c3113be605718db6942a7d12abbb70bce2a9161e
anoopms1988 <1988anoopms@gmail.com>
2015-10-12 23:42:15 +0530

    Dimensions

--- console/general/views.py
+++ console/general/views.py
@@ -6,12 +6,17 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.conf import settings
 from django.core import serializers
 from console.login.models import Company, CarType, Car, Variant, Fuel, Dealer, Assistance
+from .models import Dimensions
+from inspect import getmembers
+from pprint import pprint
 
 
 class SpecificationView(View):
     def get(self, request, *args, **kwargs):
-        variant_id=request.GET.get('id')
-        variant=Variant.objects.get(id=variant_id)
-        return render(request, 'general/specifications.html', {'variant':variant})
-
-
+        variant_id = request.GET.get('id')
+        variant = Variant.objects.get(id=variant_id)
+        try:
+            dimensions = Dimensions.objects.get(variant=variant)
+        except Dimensions.DoesNotExist:
+            dimensions = None
+        return render(request, 'general/specifications.html', {'variant': variant, 'dimensions': dimensions})
