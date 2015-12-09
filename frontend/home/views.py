75e3e5917383bb4bb4b8638d0842711cdfd1a57a
anoopms1988 <1988anoopms@gmail.com>
2015-12-09 22:26:17 +0530

    Layout and frontend company logos

--- frontend/home/views.py
+++ frontend/home/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, render_to_response
 from django.views.generic import View
 from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
 from django.contrib import messages
+from console.login.models import Company
 
 class DashboardView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'general/home.html', {})
+        companies = Company.objects.exclude(is_active=False)
+        return render(request, 'general/home.html', {'companies': companies})
